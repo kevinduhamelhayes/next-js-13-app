ff8eab1ef2e499d10d2a81086fbdd8c007b59c59
Kevin Duhamel Hayes <kevinduhamelh@gmail.com>
2023-09-12 06:16:57 -0300

    notas

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,10 @@
-export default function Home () {
+export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <div className='z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex'>
-        <p className='text-red-500'>hola</p>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+        <p className="text-red-500">hola</p>
       </div>
     </main>
   )
 }
+// hola yo del futuro este proyecto esta creado con next13 a los fines de explicar y practicar la paginacion tailwind esta en funcionamiento y se puede ver en el archivo globals.css, cada carpeta en next debe tener el nombre de la ruta y el archivo page que es el que se renderizara en la ruta puedes acceder a params para hacerlo dinamico pero no puedes anidarlo salvo carpeta dentro de carpeta
