c073968c2928f166adb02e450638fe3156e9dcff
Kevin Duhamel Hayes <kevinduhamelh@gmail.com>
2023-09-12 06:13:53 -0300

    more

--- app/post/page.jsx
+++ app/post/page.jsx
@@ -1,9 +1,15 @@
 const fetchPosts = () => {
-  return fetch ("https://jsonplaceholder.typicode.com/posts", { cache: "no-store" })
-    .then((response) => response.json()
-    
-  )
+  return fetch("https://jsonplaceholder.typicode.com/posts", {
+    cache: "no-store",
+  }).then((response) => response.json())
 }
+// getStaticProps
+// return fetch("https://jsonplaceholder.typicode.com/posts").then((response) => response.json())
+// getServerSideProps
+// return fetch("https://jsonplaceholder.typicode.com/posts", { cache: "no-store" }).then((response) => response.json())
+// incremental static regeneration
+// return fetch("https://jsonplaceholder.typicode.com/posts", {next: {revalidate: 60}}).then((response) => response.json())
+ 
 export default async function PostsPage({ params }) {
   const posts = await fetchPosts()
   return (
